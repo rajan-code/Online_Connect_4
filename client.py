f309ea26cea31dd4a7f5126fb1b389100eacb7bd
rajan-code <65838883+rajan-code@users.noreply.github.com>
2021-08-09 00:12:45 -0400

    Implemented going back to main menu after game is over

--- client.py
+++ client.py
@@ -17,7 +17,7 @@ WHITE = (255, 255, 255)
 
 SQUARE_SIZE = 100
 WIDTH = NUM_COLUMNS * SQUARE_SIZE
-HEIGHT = (NUM_ROWS + 2) * SQUARE_SIZE
+HEIGHT = (NUM_ROWS + 2) * SQUARE_SIZE + SQUARE_SIZE
 RADIUS = (SQUARE_SIZE // 2) - 5
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Client')
@@ -29,7 +29,7 @@ class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # self.server = "172.105.20.159"
-        self.server = '192.168.0.24'
+        self.server = 'localhost'
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
@@ -59,7 +59,7 @@ class Network:
 def draw_board():
     pygame.draw.rect(screen, BLACK,
                      (0, 0, WIDTH, SQUARE_SIZE))  # black rect at top of screen
-    pygame.draw.rect(screen, BLACK, (0, HEIGHT - SQUARE_SIZE, WIDTH,
+    pygame.draw.rect(screen, BLACK, (SQUARE_SIZE, HEIGHT - SQUARE_SIZE, WIDTH,
                                      SQUARE_SIZE))  # black rect at bottom of screen
     pygame.draw.rect(screen, BLUE,
                      (0, SQUARE_SIZE, WIDTH, HEIGHT - (SQUARE_SIZE * 2)))
@@ -70,7 +70,7 @@ def draw_board():
                 c * SQUARE_SIZE + SQUARE_SIZE // 2,
                 r * SQUARE_SIZE + SQUARE_SIZE // 2), RADIUS)
     pygame.draw.rect(screen, BLACK,
-                     (0, HEIGHT - SQUARE_SIZE, WIDTH, SQUARE_SIZE))
+                     (0, HEIGHT - SQUARE_SIZE*2, WIDTH, SQUARE_SIZE))
     pygame.display.update()
 
 
@@ -89,13 +89,11 @@ def updateBoard(game, row: int, col: int, player: int) -> None:
                            HEIGHT // 2 - text.get_height() // 2))
     else:
         if player == 1:
-            pygame.draw.circle(screen, RED, (
-                col * SQUARE_SIZE + SQUARE_SIZE // 2,
-                HEIGHT - (row + 2) * SQUARE_SIZE + SQUARE_SIZE // 2), RADIUS)
+            pygame.draw.circle(screen, RED, (col * SQUARE_SIZE + SQUARE_SIZE // 2,
+                                             (HEIGHT - (row + 2) * SQUARE_SIZE + SQUARE_SIZE // 2) - SQUARE_SIZE), RADIUS)
         else:
             pygame.draw.circle(screen, YELLOW, (
-                col * SQUARE_SIZE + SQUARE_SIZE // 2,
-                HEIGHT - (row + 2) * SQUARE_SIZE + SQUARE_SIZE // 2), RADIUS)
+                col * SQUARE_SIZE + SQUARE_SIZE // 2, (HEIGHT - (row + 2) * SQUARE_SIZE + SQUARE_SIZE // 2) - SQUARE_SIZE), RADIUS)
     pygame.display.update()
 
 
@@ -156,33 +154,30 @@ def main():
         while run:
             clock.tick(30)
 
-            if not (msg in ['0_move', '1_move'] and int(msg[0]) == player):
-                print('message: ', msg)
-                print(player)
+            # print('message: ', msg)
+            # print('player: ', player)
 
-            if msg in ['0_move', '1_move'] and int(msg[0]) == player:  # this player must move
-                # msg = n.client.recv(1024).decode('utf-8')  # '0_move' or '1_move'
-                # print(msg)
-                # n.client.send('a'.encode('utf-8'))
-                # game.turn = int(msg[0])
-                # print('Turn: ', game.turn)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if msg in ['0_move', '1_move'] and int(msg[0]) == player:  # this player must move
+                    # msg = n.client.recv(1024).decode('utf-8')  # '0_move' or '1_move'
+                    # print(msg)
+                    # n.client.send('a'.encode('utf-8'))
+                    # game.turn = int(msg[0])
+                    # print('Turn: ', game.turn)
 
-                for event in pygame.event.get():
-                    # print('here3')
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        sys.exit()
+                        # print('here3')
 
                     if event.type == pygame.MOUSEMOTION:
                         # print('mouse moved')
                         pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
                         x_pos = event.pos[0]
                         if player == 0:
-                            pygame.draw.circle(screen, RED,
-                                               (x_pos, SQUARE_SIZE // 2), RADIUS)
+                            pygame.draw.circle(screen, RED, (x_pos, SQUARE_SIZE // 2), RADIUS)
                         else:
-                            pygame.draw.circle(screen, YELLOW,
-                                               (x_pos, SQUARE_SIZE // 2), RADIUS)
+                            pygame.draw.circle(screen, YELLOW, (x_pos, SQUARE_SIZE // 2), RADIUS)
                         pygame.display.update()
 
                     if event.type == pygame.MOUSEBUTTONDOWN:
@@ -192,7 +187,7 @@ def main():
                         # check if move is valid TODO
                         if game.is_valid_location(column):
                             label = font1.render("Opponent's turn", 1, player_to_colour[int(not player)])
-                            screen.blit(label, (15, HEIGHT - 75))
+                            screen.blit(label, (15, HEIGHT - 75 - SQUARE_SIZE))
                             pygame.display.update()
                             print("Opponent's turn...")
                             pending_move = str(player) + ':' + str(column)  # e.g. '0:3' player 0 places piece in col 3
@@ -203,62 +198,81 @@ def main():
                             # print(game.board)
                             n.client.send(pending_move.encode('utf-8'))
                             print('Client sent:', pending_move)
-                            msg = n.client.recv(1024).decode('utf-8')  # '0_move' or '1_move'
-                            game.turn = int(not player)
-                            print('Client received1: ', msg)
-                            # player_who_just_moved, row, col = int(msg[0]), int(msg[3]), int(msg[5])
-                            # game.drop_piece(row, col, player_who_just_moved + 1)
-                            # updateBoard(game, row, col, player_who_just_moved + 1)
-                            # game.print_board(game.board)
-                            # break
 
-            else:
-                game.turn = int(not player)
-                label = font1.render("Your turn", 1, player_to_colour[player])
-                msg = n.client.recv(1024).decode('utf-8')  # 1:(0,1)
-                print('Client received2: ', msg)
+                            peek = n.client.recv(1024, socket.MSG_PEEK).decode('utf-8')
+                            print('peek:', peek)
+                            pygame.event.pump()
+                            if '_move' in peek:
+                                msg = n.client.recv(1024).decode('utf-8')  # '0_move' or '1_move'
+                                game.turn = int(not player)
+                                print('Client received1: ', msg)
+                            if len(peek) == 7 and 0 <= int(peek[0]) <= 1 and 0 <= int(peek[3]) <= 5 and 0 <= int(peek[5]) <= 6:
+                                msg = n.client.recv(1024).decode('utf-8')
+                                print('Client received1: ', msg)
 
-            if len(msg) == 7 and 0 <= int(msg[0]) <= 1 and 0 <= int(msg[3]) <= 5 and 0 <= int(msg[5]) <= 6:  # received player:(row,col)
-                player_who_just_moved, row, col = int(msg[0]), int(msg[3]), int(msg[5])
-                game.drop_piece(row, col, player_who_just_moved + 1)
-                updateBoard(game, row, col, player_who_just_moved + 1)
-                game.print_board(game.board)
-                game.turn = int(not player_who_just_moved)  # can remove game.turn stuff
-                msg = n.client.recv(1024).decode('utf-8')  # '0_move' or '1_move'
-                print('Client received3: ', msg)
-            if len(msg) == 6 and 'WON' in msg:
-                game_winner = int(msg[1])  # 0 or 1
-                if game_winner == player:
-                    label = font1.render("You Won!", 1, player_to_colour[player])
+                                # player_who_just_moved, row, col = int(msg[0]), int(msg[3]), int(msg[5])
+                                # game.drop_piece(row, col, player_who_just_moved + 1)
+                                # updateBoard(game, row, col, player_who_just_moved + 1)
+                                # game.print_board(game.board)
+                                # break
+
                 else:
-                    label = font1.render("You lost", 1, player_to_colour[player])
-                run = False
-                pygame.draw.rect(screen, BLACK, (0, HEIGHT - SQUARE_SIZE, WIDTH, SQUARE_SIZE))
-                screen.blit(label, (15, HEIGHT - 75))
-                pygame.display.update()
-                pygame.time.wait(1000)
-                menu_screen()
-                # run = False
+                    game.turn = int(not player)
+                    label = font1.render("Your turn", 1, player_to_colour[player])
+                    msg = n.client.recv(1024).decode('utf-8')  # 1:(0,1)
+                    print('Client received2: ', msg)
 
-            # updateBoard(game, 0, 3, 0)
-            pygame.draw.rect(screen, BLACK,(0, HEIGHT - SQUARE_SIZE, WIDTH, SQUARE_SIZE))
-            screen.blit(label, (15, HEIGHT - 75))
-            pygame.display.update()
+                if len(msg) == 7 and 0 <= int(msg[0]) <= 1 and 0 <= int(msg[3]) <= 5 and 0 <= int(msg[5]) <= 6:  # received player:(row,col)
+                    player_who_just_moved, row, col = int(msg[0]), int(msg[3]), int(msg[5])
+                    game.drop_piece(row, col, player_who_just_moved + 1)
+                    updateBoard(game, row, col, player_who_just_moved + 1)
+                    game.print_board(game.board)
+                    game.turn = int(not player_who_just_moved)  # can remove game.turn stuff
+                    peek = n.client.recv(1024, socket.MSG_PEEK).decode('utf-8')
+                    if peek[1:] == '_move':
+                        msg = n.client.recv(1024).decode('utf-8')  # '0_move' or '1_move'
+                        print('Client received3: ', msg)
+                if len(msg) == 6 and 'WON' in msg:
+                    game_winner = int(msg[1])  # 0 or 1
+                    if game_winner == player:
+                        label = font1.render("You Won! Click to play again.", 1, player_to_colour[player])
+                    else:
+                        label = font1.render("You lost. Click to play again.", 1, player_to_colour[player])
 
-            """
-            if game.bothWent():
-                # redrawWindow(win, game, player)
-                pygame.time.delay(500)
-                try:
-                    game = n.send("reset")
-                except:
-                    run = False
-                    print("Couldn't get game")
-                    break
-    
+                    pygame.draw.rect(screen, BLACK, (0, HEIGHT - SQUARE_SIZE*2, WIDTH, SQUARE_SIZE))
+                    screen.blit(label, (15, HEIGHT - 75 - SQUARE_SIZE))
+                    pygame.display.update()
+                    run2 = True
+                    while run2:
+                        # pygame.time.wait(3000)
+                        for event in pygame.event.get():
+                            if event.type == pygame.QUIT:
+                                pygame.quit()
+                                sys.exit()
+                            if event.type == pygame.MOUSEBUTTONDOWN:
+                                run2 = False
+                                menu_screen()
+                    # run = False
+
+                # updateBoard(game, 0, 3, 0)
+                pygame.draw.rect(screen, BLACK, (0, HEIGHT - SQUARE_SIZE*2, WIDTH, SQUARE_SIZE))
+                screen.blit(label, (15, HEIGHT - 75 - SQUARE_SIZE))
                 pygame.display.update()
-                pygame.time.delay(2000)
-            """
+
+                """
+                if game.bothWent():
+                    # redrawWindow(win, game, player)
+                    pygame.time.delay(500)
+                    try:
+                        game = n.send("reset")
+                    except:
+                        run = False
+                        print("Couldn't get game")
+                        break
+        
+                    pygame.display.update()
+                    pygame.time.delay(2000)
+                """
 
     #  for event in pygame.event.get():
     #      if event.type == pygame.QUIT:
